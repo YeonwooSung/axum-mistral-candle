3dd0c181c656c6ed6bd3c8d23a65f721a975f9bd
YeonwooSung <neos960518@gmail.com>
2025-07-03 21:39:54 +0900

    refactor: Move axum mistral codes into corresponding dir

--- axum_mistral/backend/src/main.rs
+++ axum_mistral/backend/src/main.rs
@@ -10,12 +10,10 @@ use websockets::server::server;
 #[tokio::main]
 async fn main() {
     // Load Mistral
-    let config_string = std::fs
-        ::read_to_string("./config_model.yaml")
-        .expect("Load config_model.yaml");
-    let args_to_load_model: ArgsToLoadModel = serde_yaml
-        ::from_str(config_string.as_str())
-        .expect("config_model.yaml to struct");
+    let config_string =
+        std::fs::read_to_string("./config_model.yaml").expect("Load config_model.yaml");
+    let args_to_load_model: ArgsToLoadModel =
+        serde_yaml::from_str(config_string.as_str()).expect("config_model.yaml to struct");
     let model_args = Mutex::new(load_model(args_to_load_model).unwrap());
 
     // Start server
